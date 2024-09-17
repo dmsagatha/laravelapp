3b06fab41defcc129d8c96a51c2823c30d066a79
Doris Morales <dmorales55@gmail.com>
2024-09-17 15:19:16 -0500

    Tema Ocuro/Claro con Tailwind CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     </style>
   </head>
   <body class="font-sans antialiased">
-    <div class="min-h-screen bg-slate-50 dark:bg-slate-900">
+    <div class="min-h-screen bg-slate-50 dark:bg-slate-800">
       @include('layouts.navigation')
 
       <!-- Page Heading -->
