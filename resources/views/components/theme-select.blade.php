07316412cf79ba29172f0285823071d447d43082
Doris Morales <dmorales55@gmail.com>
2024-09-17 14:10:10 -0500

    Tema Ocuro o Claro con Tailwind css

--- resources/views/components/theme-select.blade.php
+++ resources/views/components/theme-select.blade.php
@@ -1,11 +1,11 @@
 <button id="theme-toggle" class="py-2 px-4 bg-gray-200 dark:bg-gray-700 rounded focus:outline-none">
   <!-- Ícono del sol (mostrar cuando esté en tema oscuro para cambiar a tema claro) -->
   <svg id="icon-sun" class="h-6 w-6 hidden dark:block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v2m4.219 1.781l1.415 1.414m3.248 3.248h2m-2.219 4.219l-1.415 1.414m-4.781 4.781v2m-4.219-1.781l-1.414-1.415m-3.248-3.248H3m1.781-4.219l1.414-1.415M12 7a5 5 0 110 10 5 5 0 010-10z" />
+    <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd" />
   </svg>
 
   <!-- Ícono de la luna (mostrar cuando esté en tema claro para cambiar a tema oscuro) -->
   <svg id="icon-moon" class="h-6 w-6 block dark:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.293 9.293a8 8 0 11-11.586 11.586A9 9 0 1017.293 9.293z" />
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
   </svg>
 </button>
