356b11d53dfe9c67566d5f2a0e161a13999359b8
Doris Morales <dmorales55@gmail.com>
2024-09-30 10:29:24 -0500

    DataTables.Net JQuery con Tailwind CSS

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,6 +17,6 @@ class UserSeeder extends Seeder
       'password' => Hash::make('superadmin')
     ]);
 
-    User::factory(10)->create();
+    User::factory(50)->create();
   }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-slate-800 border-b border-slate-100 dark:border-slate-700">
+<nav x-data="{ open: false }" class="bg-white dark:bg-slate-800 border-b border-slate-100 dark:border-slate-700 fixed w-full z-20 top-0 start-0">
   <!-- Primary Navigation Menu -->
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex justify-between h-16">
