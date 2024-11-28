da519e6be08715ea4e8567317622a7f0b1a435c9
Doris Morales <dmorales55@gmail.com>
2024-11-27 22:14:44 -0500

    CRUD Memorias y Selects con constantes

--- resources/views/admin/memories/_fields.blade.php
+++ resources/views/admin/memories/_fields.blade.php
@@ -11,12 +11,4 @@
       <x-input-select name="capacity" :options="$capacities" :selected="old('capacity', $memory->capacity ?? '')" label="Capacidades" placeholder=" " />
     </div>
   </div>
-
-  {{-- <div class="col-span-6 sm:col-span-3 md:col-span-2">
-    <div class="relative z-0 group mt-2">
-      <x-input-label for="capacity" :value="__('Capacidades')" />
-      <x-select-label name="capacity" id="selectCapacity" :options="$capacities" label="Capacidad" />
-      <x-input-error :messages="$errors->get('capacity')" />
-    </div>
-  </div> --}}
 </div>
--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,14 +1,15 @@
 @props(['name', 'options', 'selected', 'label'])
+
 @if (!empty($label))
-    <label for="{{ $id ?? $name }}" class="select--label required {{ $class ?? '' }}">
-        {{ $label }}
-    </label>
+  <label for="{{ $id ?? $name }}" class="select--label required {{ $class ?? '' }}">
+    {{ $label }}
+  </label>
 @endif
 
 <select name="{{ $name }}" id="{{ $name }}" {{ $attributes->merge(['class' => 'select--control' . ($errors->has($name) ? ' border-red-500' : '')]) }}>
   <option selected value="">Seleccionar</option>
   @foreach ($options as $value => $label)
-    <option value="{{ $value }}" {{ old($name, $selected) == $value ? 'selected' : '' }}>
+    <option value="{{ $value }}" {{ old($name, $selected)==$value ? 'selected' : '' }}>
       {{ $label }}
     </option>
   @endforeach
