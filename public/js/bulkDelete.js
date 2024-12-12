2164d43eff76f185063d32add1e42c180b772174
Doris Morales <dmorales55@gmail.com>
2024-12-11 23:25:43 -0500

    Barra de navegacion con menu desplegable

--- public/js/bulkDelete.js
+++ public/js/bulkDelete.js
@@ -1,10 +1,10 @@
 // [7] Cómo Seleccionar Todo con Checkbox en JavaScript - https://www.youtube.com/watch?v=8rxxUhPPna0
-  const selectedAll = document.querySelector("#selectedAll")
-  const itemSelected = document.querySelectorAll('#selectIds')
-  const bulkDeleteButton = document.querySelector("#bulkDeleteButton")
+  const selectedAll = document.querySelector("#selectedAll");
+  const itemSelected = document.querySelectorAll('#selectIds');
+  const bulkDeleteButton = document.querySelector("#bulkDeleteButton");
 
   // Convertir a un arreglo
-  const itemSelectedArray = Array.from(itemSelected)
+  const itemSelectedArray = Array.from(itemSelected);
 
   // Evento al dar clic en el botón de "Todos" (2 opciones)
   /* selectedAll.addEventListener('change', (event) => {
@@ -48,6 +48,53 @@
       }
     });
   });
+
+  // Confirmar eliminación
+  // [8] Eliminar registro de mysql con php y javascript PASO a PASO - https://www.youtube.com/watch?v=B3JGwKxCAIQ
+  bulkDeleteButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const confirmDelete = confirm('¿Estás seguro de eliminar los items seleccionados?')
+
+    if (confirmDelete) {
+      /* const itemChecked = itemSelectedArray.filter(item => item.checked);
+      const itemValues = itemChecked.map(item => item.value);
+      console.log(itemValues); */
+
+      const itemChecked = itemSelectedArray
+        .filter(item => item.checked)
+        .map(item => item.value);
+      console.log(itemChecked);
+
+      // Eliminar los items seleccionados
+      //... (Código para eliminar los items seleccionados en tu base de datos)
+      // Luego de eliminarlos, volver a ocultar el botón de Eliminar
+      //...
+
+      bulkDeleteButton.classList.remove('flex');
+      bulkDeleteButton.classList.add('hidden');
+
+      // Limpiar los checks
+      itemSelectedArray.forEach((item) => {
+        item.checked = false;
+      });
+
+      alert('Se eliminan los items seleccionados!');
+    }
+    
+    /* if (confirm('¿Estás seguro de eliminar los items seleccionados?')) {
+      itemSelected.forEach((item) => {
+        if (item.checked) {
+          item.parentElement.removeChild(item);
+        }
+      });
+    } */
+  });
+
+  // Ocultar el botón de Eliminar si no hay items seleccionados al inicio
+  /* if (!Array.from(itemSelected).some((chkbx) => chkbx.checked)) {
+    bulkDeleteButton.classList.remove('flex');
+    bulkDeleteButton.classList.add('hidden');
+  } */
   
 
 
