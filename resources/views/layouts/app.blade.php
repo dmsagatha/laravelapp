cbe056a1c57423762cd0b54aedccf787d5a489be
Doris Morales <dmorales55@gmail.com>
2024-12-12 19:15:00 -0500

    Barra de navegacion con menu desplegable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
     @stack('styles')
   </head>
-  <body class="bg-slate-50 dark:bg-slate-900 antialiased font-sans">
+  <body class="bg-slate-100 dark:bg-slate-900 antialiased font-sans">
     <div class="min-h-screen text-slate-700 dark:text-slate-300">
       @include('layouts.navigation')
 
