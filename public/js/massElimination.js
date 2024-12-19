7736ad199b49b5882cd1147ab6fc38be9926371f
Doris Morales <dmorales55@gmail.com>
2024-12-19 12:27:53 -0500

    Usuarios: Eliminacion y restauracion masiva con modal

--- public/js/massElimination.js
+++ public/js/massElimination.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
       checkboxes.forEach(checkbox => {
         checkbox.checked = selectAllCheckbox.checked;
       });
+      
       updateDeleteButtonVisibility();
     });
   }
