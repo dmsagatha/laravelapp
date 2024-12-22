2c239dabc8811c5443dd5619102963b907c6dc66
Doris Morales <dmorales55@gmail.com>
2024-12-22 18:52:58 -0500

    Componente Blade de Alert + Alpine Js

--- app/View/Components/Alert.php
+++ app/View/Components/Alert.php
@@ -17,16 +17,6 @@ class Alert extends Component
     $this->message = $message;
   }
 
-  public function backgroundCSS()
-  {
-    return [
-      'success' => 'bg-success-500 text-slate-50 border-success-700',
-      'info'    => 'bg-info-500 text-slate-50 border-info-700',
-      'error'   => 'bg-error-500 text-slate-50 border-error-700',
-      'warning' => 'bg-warning-500 text-slate-50 border-warning-700',
-    ][$this->type];
-  }
-
   public function render(): View|Closure|string
   {
     return view('components.alert');
