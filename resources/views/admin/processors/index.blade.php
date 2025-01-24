921907c89b3a55e76b9ffb836c13baef19cd3320
Doris Morales <dmorales55@gmail.com>
2025-01-24 11:19:00 -0500

    Procesadores-Prototipos: Selects dependientes

--- resources/views/admin/processors/index.blade.php
+++ resources/views/admin/processors/index.blade.php
@@ -5,87 +5,80 @@
     </h2>
   </x-slot>
   
-  <div class="flow-root mx-auto w-full md:w-full lg:w-4/5 sm:px-6 lg:px-8 my-12">
-    <div class="bg-slate-50 dark:bg-slate-800 overflow-hidden shadow-sm sm:rounded-lg">
-      <div class="px-6 text-slate-900 dark:text-slate-100">
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <div class="px-4 py-5 mx-auto text-center max-w-7xl sm:px-6 lg:py-2 lg:px-8">
-            <h2 class="text-2xl font-extrabold tracking-tight text-slate-900 dark:text-slate-50 sm:text-3xl">
-              <span class="block">
-                <a href="https://datatables.net/extensions/responsive/examples/initialisation/default.html"
-                  target="_blank">
-                  Importar Procesadores y Memorias RAM - <br>
-                  Relación Muchos a Muchos
-                </a>
-              </span>
-            </h2>
-            <span class="block py-4">Guía: 
-              <a href="https://www.youtube.com/watch?v=Q2AUH9w9XaA" target="_new" class="font-bold text-lg text-indigo-500" alt="Tailwind CSS">
-                Laravel Excel Import to Database with Errors and Validation Handling
-              </a>
-            </span>
+  <div class="flow-root mx-auto w-full max-w-6xl">
+    <div class="text-slate-900 bg-slate-50 dark:text-slate-100 dark:bg-slate-800 p-4 m-2 sm:p-2 shadow rounded">
+      <div class="relative overflow-x-auto w-full mx-auto p-4 m-4 text-center sm:px-6 lg:py-2 lg:px-8">
+        <h2 class="text-2xl font-extrabold tracking-tight text-slate-900 dark:text-slate-50 sm:text-3xl">
+          <span class="block">
+            <a href="https://datatables.net/extensions/responsive/examples/initialisation/default.html"
+              target="_blank">
+              Importar Procesadores y Memorias RAM - <br>
+              Relación Muchos a Muchos
+            </a>
+          </span>
+        </h2>
+        <span class="block py-4">Guía: 
+          <a href="https://www.youtube.com/watch?v=Q2AUH9w9XaA" target="_new" class="font-bold text-lg text-indigo-500" alt="Tailwind CSS">
+            Laravel Excel Import to Database with Errors and Validation Handling
+          </a>
+        </span>
 
-            <form action="{{ route('processors.import') }}" method="POST" enctype="multipart/form-data">
-              @csrf
-              {{-- <input type="file" id="import_file" name="import_file" accept=".xls" required> --}}
-              <input type="file" id="import_file" name="import_file" required>
-              
-              <button type="submit" class="ml-3 bg-indigo-600 dark:bg-indigo-500 text-white font-medium hover:bg-indigo-500 dark:hover:bg-indigo-600 px-4 py-2 rounded-md">
-                Importar
-              </button>
-            </form>
-            <span class="block py-2">
-              (Archivo de ejmplo: public/importar/processors.xlsx) - <br>
-              Intentar subir varias veces el mismo archivo y verá los resultados
-            </span>
-          </div>
-        </div>
+        <form action="{{ route('processors.import') }}" method="POST" enctype="multipart/form-data">
+          @csrf
+          
+          <input type="file" id="import_file" name="import_file" required>
+          
+          <button type="submit" class="ml-3 bg-indigo-600 dark:bg-indigo-500 text-white font-medium hover:bg-indigo-500 dark:hover:bg-indigo-600 px-4 py-2 rounded-md">
+            Importar
+          </button>
+        </form>
+        <span class="block py-2">
+          (Archivo de ejmplo: public/importar/processors.xlsx) - <br>
+          Intentar subir varias veces el mismo archivo y verá los resultados
+        </span>
+      </div>
 
-        <div class="text-white h-full bg-gray-800">
-          <h1 class="text-2xl md:text-4xl lg:text-5xl font-extrabold pt-8 px-2 text-center w-full">Tailwind CSS Premier League Table #1</h1>
-          <div class="py-8">
-            <div class="max-w-screen-xl px-2 mx-auto">
-              <table id="dtTheme" class="w-full text-base">
-                <thead>
-                  <tr>
-                    <th width="1%">N°</th>
-                    <th>Usuario</th>
-                    <th>MAC</th>
-                    <th>Service Tag</th>
-                    <th>Memorias</th>
-                    <th>Prototipos</th>
-                    <th>Acciones</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  @foreach ($processors as $key => $item)
-                    <tr>
-                      <td style="text-align: center;">{{ $key + 1 }}</td>
-                      <th>{{ $item->user->name }}</th>
-                      <th>{{ $item->mac }}</th>
-                      <td>{{ $item->servicetag }}</td>
-                      <td class="text-xs">
-                        @if(!$item->addMemories->isEmpty())
-                          <p><span class="underline">Adicionales:</span></p>
-                          @foreach($item->addMemories as $addMemory)
-                            {{ $addMemory->brand }} - {{ $addMemory->technology }} - {{ $addMemory->velocity }} MHz - {{ $addMemory->capacity }} x {{ $addMemory->pivot->quantity_addmem }}<br>
-                          @endforeach
-                        @endif
-                      </td>
-                      <td>{{ $item->prototype->reference }}</td>
-                      <td>
-                        <div class="flex items-stretch justify-center">
-                          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-                          <a href="#"  class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Eliminar</a>
-                        </div>
-                      </td>
-                    </tr>
-                  @endforeach
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+      <div class="relative overflow-x-auto max-w-5xl mx-auto p-4 m-4 shadow-sm shadow-slate-300 sm:rounded-lg">
+        @include('partials.failures')
+        
+        <table id="dtTheme" class="display compact nowrap row-border stripe">
+          <thead>
+            <tr>
+              <th width="1%">N°</th>
+              <th>Usuario</th>
+              <th>MAC</th>
+              <th>Service Tag</th>
+              <th>Memorias</th>
+              <th>Prototipos</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($processors as $key => $item)
+              <tr>
+                <td style="text-align: center;">{{ $key + 1 }}</td>
+                <th>{{ $item->user->name }}</th>
+                <th>{{ $item->mac }}</th>
+                <td>{{ $item->servicetag }}</td>
+                <td class="text-xs">
+                  @if(!$item->addMemories->isEmpty())
+                    <p><span class="underline">Adicionales:</span></p>
+                    @foreach($item->addMemories as $addMemory)
+                      {{ $addMemory->brand }} - {{ $addMemory->technology }} - {{ $addMemory->velocity }} MHz - {{ $addMemory->capacity }} x {{ $addMemory->pivot->quantity_addmem }}<br>
+                    @endforeach
+                  @endif
+                </td>
+                <td>{{ $item->prototype->reference }}</td>
+                <td>
+                  <div class="flex items-stretch justify-center">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+                    <a href="#"  class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Eliminar</a>
+                  </div>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
