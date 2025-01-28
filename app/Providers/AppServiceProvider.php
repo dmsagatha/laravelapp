b9a4e64f5eeef9794c48a55b4c4b8383bdf64851
Doris Morales <dmorales55@gmail.com>
2025-01-27 19:58:55 -0500

    CRUD Procesadores-Memorias: Selects dependientes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
+    // Para evitar que Laravel cargue las relaciones lazientes en las consultas (diferidas)
+    Model::preventLazyLoading();
+
     Route::resourceVerbs([
         'create' => 'crear',
         'edit' => 'editar'
