84df936e59e45eecc7281d2db7199880406db825
Doris Morales <dmorales55@gmail.com>
2025-02-01 14:50:03 -0500

    CRUD Procesadores-Memorias: Selects dependientes

--- resources/views/admin/processors/createUpdate.blade.php
+++ resources/views/admin/processors/createUpdate.blade.php
@@ -83,14 +83,20 @@
               <select name="memories[${index}][id]" class="select--control sm:w-80 md:w-60 p-2" required>
                 <option value="">Seleccionar</option>
                 @foreach($memories as $memory)
-                  <option value="{{ $memory->id }}">{{ $memory->serial }} - {{ $memory->capacity }}GB</option>
+                  <option value="{{ $memory->id }}">
+                    {{ $memory->serial }} - {{ $memory->technology }} - {{ $memory->capacity }}
+                  </option>
                 @endforeach
                 </select>
             </td>
             <td>
               <input type="number" name="memories[${index}][quantity]" class="block py-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer text-center" min="1" required>
             </td>
-            <button type="button" class="remove-memory-btn bg-red-500 text-white px-2 py-1 rounded">Eliminar</button>
+            <button type="button" class="remove-memory-btn bg-red-500 text-white px-2 py-1 rounded">
+              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+              </svg>
+            </button>
           `;
           memoryList.appendChild(memoryField);
         });
